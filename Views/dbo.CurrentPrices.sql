da788a08569f5315a4707d3a5ad2eeb9543a08b2
vikas-kattaria <vikas.kattaria@thepsi.com>
2025-11-18 14:05:50 +0530

    First time changes

---
+++ Views/dbo.CurrentPrices.sql
@@ -0,0 +1,14 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE VIEW [dbo].[CurrentPrices]
+AS
+    SELECT  WidgetID ,
+            Price ,
+            Description
+    FROM    Widgets
+            INNER JOIN WidgetPrices ON Widgets.RecordID = WidgetID;
+
+GO
